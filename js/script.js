91fcc585745dc7ab10b48abceebbe73d9be89eaf
Romashka <rroma13579@gmail.com>
2022-09-04 23:12:06 +0100

    second commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var add_btn = document.querySelector(".add-more");
+var statistic_block = document.querySelector(".statistic-block");
+function Click(){
+    statistic_block.innerHTML += "<input class='block-txt' placeholder='NoNe'></input>";
+}
