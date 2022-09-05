a899a53cff4b7e46b9dcf581e4c2359a7d1c3a1f
Romashka <rroma13579@gmail.com>
2022-09-05 23:47:21 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var add_btn = document.querySelector(".add-more");
 var statistic_block = document.querySelector(".statistic-block");
-var blocks_txt = document.querySelectorAll(".block-txt")
+var statistic = document.querySelector(".statistic");
+var blocks_txt = document.querySelectorAll(".block-txt");
+var procents = document.querySelectorAll(".procent");
+var k = 1
 
-function Click(){
+function ClickAdd(){
+    k++
     var clone_block = blocks_txt[0].cloneNode(true);
     statistic_block.prepend(clone_block);
     blocks_txt = document.querySelectorAll(".block-txt");
+
+    var clone_procent = procents[0].cloneNode(true);
+    statistic.prepend(clone_procent);
+    procents = document.querySelectorAll(".procent");
+    statistic.style.gridTemplateColumns = "repeat("+k+", 1fr)"
 }
