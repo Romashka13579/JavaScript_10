a7901f1ddd553950d05f7098c338c39fb31169de
Romashka <rroma13579@gmail.com>
2022-09-05 23:31:42 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var add_btn = document.querySelector(".add-more");
 var statistic_block = document.querySelector(".statistic-block");
+var blocks_txt = document.querySelectorAll(".block-txt")
+
 function Click(){
-    statistic_block.innerHTML += "<input class='block-txt' placeholder='NoNe'></input>";
+    var clone_block = blocks_txt[0].cloneNode(true);
+    statistic_block.prepend(clone_block);
+    blocks_txt = document.querySelectorAll(".block-txt");
 }
